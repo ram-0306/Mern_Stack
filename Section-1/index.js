2948ee1fdc8181a505a2c43686d4d71d70845fff
Ramgopal Gautam <ramgopalgautam456@gmail.com>
2024-02-16 13:54:36 +0530

    js

---
+++ Section-1/index.js
@@ -0,0 +1,12 @@
+console.log("welcome to javascript");
+a=29;
+myName = "Ram Gopal";
+isHungry = true;
+empty = null;
+
+console.log(myName);
+console.log(a);
+console.log( typeof myName);
+console.log(typeof(a));
+
+
---
+++ Section-1/keywords.js
@@ -0,0 +1,12 @@
+if( 34%2 === 0 ){
+
+console.log("even");
+
+abc='nice';
+
+}else{
+
+    console.log("odd");
+
+}
+console.log(abc);
