b980ae9ab36d577e4a5f2b96f187efb2d03e26fe
Ramgopal Gautam <ramgopalgautam456@gmail.com>
2024-02-25 17:30:45 +0530

    js

---
+++ Exercise-1/demo.js
@@ -0,0 +1,23 @@
+// const product = {
+//     name: "Pen",
+//     rating: 4,
+//     offer: 5,
+//     price: 270,
+//     isdeal: true,
+//     };
+
+
+// console.log(product);
+// product.name = "Ramgopal";
+// console.log(product);
+
+
+// console.log(Object.keys(product));
+
+// const arr = [22,45,65,78,99,102];
+// const res =arr.filter((val)=>{
+//     let sum = val>25 && val<80;
+//     return sum;
+    
+// })
+// console.log(res);
---
+++ Exercise-1/tempCodeRunnerFile.js
@@ -0,0 +1,15 @@
+const product = {
+//     name: "Pen",
+//     rating: 4,
+//     offer: 5,
+//     price: 270,
+//     isdeal: true,
+//     };
+
+
+// console.log(product);
+// product.name = "Ramgopal";
+// console.log(product);
+
+
+// console.log(Object.keys(product));
