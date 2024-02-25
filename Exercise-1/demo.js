305062cae6986b9965ad9cfc206fd6a9da66751f
Ramgopal Gautam <ramgopalgautam456@gmail.com>
2024-02-25 20:28:43 +0530

    .

--- Exercise-1/demo.js
+++ Exercise-1/demo.js
@@ -7,17 +7,50 @@
 //     };
 
 
-// console.log(product);
-// product.name = "Ramgopal";
-// console.log(product);
-
-
 // console.log(Object.keys(product));
+
 
 // const arr = [22,45,65,78,99,102];
 // const res =arr.filter((val)=>{
 //     let sum = val>25 && val<80;
 //     return sum;
-    
 // })
 // console.log(res);
+
+
+// let str = ["ram","rohan","ravi","kishan"];
+
+// for (let s of str) {
+//   console.log(s.toUpperCase());   
+// }
+
+
+
+// Methods that made changes in exsisting array
+
+// let arr = [12, 23, 56, 67, 65, 98];
+
+// arr.push("34"); //adds element at the end of the array
+
+// console.log(arr);
+
+// let deleted = arr.pop();  //deletes element from the end and returns that element
+
+// console.log(arr);
+// console.log(deleted);
+
+//Splice method
+
+// arr.splice(1,2, 44, 55, 88);
+
+// console.log(arr);
+
+
+
+// Methods that does not made changes in exsisting array
+
+//Slice Method
+
+// console.log(arr.slice(1,3));
+
+
