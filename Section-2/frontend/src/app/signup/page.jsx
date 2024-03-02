b45da248e8d5e967c345dbf5352e3ae755459d06
Ramgopal Gautam <ramgopalgautam456@gmail.com>
2024-03-02 23:58:08 +0530

    Nextjs assignment

--- Section-2/frontend/src/app/signup/page.jsx
+++ Section-2/frontend/src/app/signup/page.jsx
@@ -3,18 +3,8 @@ import React from 'react'
 const signup = () => {
   return (
     <div>
-        <h1>Signup</h1>
-        <form>
-          <label>
-            Username
-            <input type="text" />
-          </label>
-          <label>
-            Password
-            <input type="password" />
-          </label>
-          <button type="submit" className='myBtn'>Submit</button>
-        </form>
+        
+        
     </div>
   )
 }
